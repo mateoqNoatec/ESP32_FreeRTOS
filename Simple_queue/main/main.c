00652207ea9598fef39e14ace31e6a5bfbe1c5f1
Mateo <mateo.quintero@noatec.co>
2023-02-21 10:39:22 -0500

    Simple queue
    Two tasks, the first sends a data to the second via queue and the second receives and shows it.

---
+++ Simple_queue/main/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/queue.h>
+
+QueueHandle_t queueHandle;
+
+void task1 (void *pvArgs)
+{
+    int data = 1;
+    while(1)
+    {
+        xQueueSend(queueHandle, &data, pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+void task2(void *pvArgs)
+{
+    int data=0;
+    while(1)
+    {
+        if(xQueueReceive(queueHandle, &data, pdMS_TO_TICKS(1000)))
+        {
+            printf("\n\tData received: %d\n", data);
+        }
+    }
+}
+
+void app_main(void)
+{
+    queueHandle = xQueueCreate(5,sizeof(int));
+    xTaskCreate(task1, "task1", 2048, NULL,1,NULL);
+    xTaskCreate(task2, "task2", 2048, NULL,1,NULL);
+}
